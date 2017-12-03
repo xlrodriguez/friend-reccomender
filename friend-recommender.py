ee36b48a2c483067d3d1b722128563ef4cf56712
Xiomara Rodriguez <xlrodriguez@me.com>
2017-12-03 15:55:21 -0800

    Create friend-recommender.py

---
+++ friend-recommender.py
@@ -0,0 +1,114 @@
+#!/usr/bin/env python3
+
+
+class SocialNetwork:
+
+    def __init__(self):
+        '''Constructor; initialize an empty social network
+        '''
+        self.users = {}
+
+    def list_users(self):
+        '''List all users in the network
+
+        Returns:
+            [str]: A list of usernames
+        '''
+        pass # FIXME
+
+    def add_user(self, user):
+        '''Add a user to the network
+
+        This user will have no friends initially.
+
+        Arguments:
+            user (str): The username of the new user
+
+        Returns:
+            None
+        '''
+        pass # FIXME
+
+    def add_friend(self, user, friend):
+        '''Adds a friend to a user
+
+        Note that "friends" are one-directional - this is the equivalent of
+        "following" someone.
+
+        If either the user or the friend is not a user in the network, they
+        should be added to the network.
+
+        Arguments:
+            user (str): The username of the follower
+            friend (str): The username of the user being followed
+
+        Returns:
+            None
+        '''
+        pass # FIXME
+
+    def get_friends(self, user):
+        '''Get the friends of a user
+
+        Arguments:
+            user (str): The username of the user whose friends to return
+
+        Returns:
+            [str]: The list of usernames of the user's friends
+
+        '''
+        pass # FIXME
+
+    def suggest_friend(self, user):
+        '''Suggest a friend to the user
+
+        See project specifications for details on this algorithm.
+
+        Arguments:
+            user (str): The username of the user to find a friend for
+
+        Returns:
+            str: The username of a new candidate friend for the user
+        '''
+        pass # FIXME
+
+    def to_dot(self):
+        result = []
+        result.append('digraph {')
+        result.append('    layout=neato')
+        result.append('    overlap=scalexy')
+        for user in self.list_users():
+            for friend in self.get_friends(user):
+                result.append('    "{}" -> "{}"'.format(user, friend))
+        result.append('}')
+        return '\n'.join(result)
+
+
+def create_network_from_file(filename):
+    '''Create a SocialNetwork from a saved file
+
+    Arguments:
+        filename (str): The name of the network file
+
+    Returns:
+        SocialNetwork: The SocialNetwork described by the file
+    '''
+    network = SocialNetwork()
+    with open(filename) as fd:
+        for line in fd.readlines():
+            line = line.strip()
+            users = line.split()
+            network.add_user(users[0])
+            for friend in users[1:]:
+                network.add_friend(users[0], friend)
+    return network
+
+
+def main():
+    network = create_network_from_file('simple.network')
+    print(network.to_dot())
+    print(network.suggest_friend('francis'))
+
+
+if __name__ == '__main__':
+    main()
