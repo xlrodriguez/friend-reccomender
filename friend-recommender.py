79f4efe4a5da2252621bd3f5ae80434da5ab3100
Unknown <aolavarriaga@oxy.edu>
2017-12-05 10:08:20 -0700

    all code

--- friend-recommender.py
+++ friend-recommender.py
@@ -1,78 +1,72 @@
-#!/usr/bin/env python3
-
-
 class SocialNetwork:
-
     def __init__(self):
         '''Constructor; initialize an empty social network
         '''
         self.users = {}
 
     def list_users(self):
-        '''List all users in the network
-
-        Returns:
-            [str]: A list of usernames
-        '''
-        pass # FIXME
+        self.users.keys()
+        userslist = list()
+        for i in self.users.keys():
+            userslist.append(i)
+        return userslist
 
     def add_user(self, user):
+        self.users[user] = ''
+        print(self.users)
 
-        #poop
-        '''Add a user to the network
-
-        This user will have no friends initially.
-
-        Arguments:
-            user (str): The username of the new user
-
-        Returns:
-            None
-        '''
-        pass # FIXME
+        # '''Add a user to the network
+        #
+        # This user will have no friends initially.
+        #
+        # Arguments:
+        #     user (str): The username of the new user
+        #
+        # Returns:
+        #     None
+        # '''
+        pass  # FIXME
 
     def add_friend(self, user, friend):
-        '''Adds a friend to a user
-
-        Note that "friends" are one-directional - this is the equivalent of
-        "following" someone.
-
-        If either the user or the friend is not a user in the network, they
-        should be added to the network.
-
-        Arguments:
-            user (str): The username of the follower
-            friend (str): The username of the user being followed
-
-        Returns:
-            None
-        '''
-        pass # FIXME
+        self.users[user].append(friend)
+        # '''Adds a friend to a user
+        #
+        # Note that "friends" are one-directional - this is the equivalent of
+        # "following" someone.
+        #
+        # If either the user or the friend is not a user in the network, they
+        # should be added to the network.
+        #
+        # Arguments:
+        #     user (str): The username of the follower
+        #     friend (str): The username of the user being followed
+        #
+        # Returns:
+        #     None
+        # '''
+        pass  # FIXME
 
     def get_friends(self, user):
-        '''Get the friends of a user
+        return list(self.users.values())[list(self.users.keys().index(user))]
 
-        Arguments:
-            user (str): The username of the user whose friends to return
-
-        Returns:
-            [str]: The list of usernames of the user's friends
-
-        '''
-        pass # FIXME
+      # FIXME
 
     def suggest_friend(self, user):
-        '''Suggest a friend to the user
+        # for i in self.users:
+            #nested loops
 
-        See project specifications for details on this algorithm.
 
-        Arguments:
-            user (str): The username of the user to find a friend for
-
-        Returns:
-            str: The username of a new candidate friend for the user
-        '''
-        pass # FIXME
+        # '''Suggest a friend to the user
+        #
+        # See project specifications for details on this algorithm.
+        #
+        # Arguments:
+        #     user (str): The username of the user to find a friend for
+        #
+        # Returns:
+        #     str: The username of a new candidate friend for the user
+        # '''
+        pass  #FIXME
 
     def to_dot(self):
         result = []
@@ -107,7 +101,7 @@ def create_network_from_file(filename):
 
 
 def main():
-    network = create_network_from_file('simple.network')
+    network = create_network_from_file('simple.network.txt')
     print(network.to_dot())
     print(network.suggest_friend('francis'))
 
